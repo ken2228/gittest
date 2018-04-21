cb6224eb4ca53a6a4cac442b8258adf485f3b9a1
ken2228 <danny_sleugel@hotmail.com>
2018-04-21 13:43:06 +0200

    initial commit

---
+++ src/test.java
@@ -0,0 +1,39 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sendCommand");
+
+	     /**
+	      * YOU MUST CHANGE THE FOLLOWING
+	      * FILE_NAME: A FILE IN THE DIRECTORY
+	      * USER: LOGIN USER NAME
+	      * PASSWORD: PASSWORD FOR THAT USER
+	      * HOST: IP ADDRESS OF THE SSH SERVER
+	     **/
+	     String command = "ls oor";
+	     String userName = "pi";
+	     String password = "raspberry";
+	     String connectionIP = "192.168.137.2";
+	     SSHManager instance = new SSHManager(userName, password, connectionIP, "");
+	     String errorMessage = instance.connect();
+
+	     if(errorMessage != null)
+	     {
+	        System.out.println(errorMessage);
+	       
+	     }
+
+	     String expResult = "FILE_NAME\n";
+	     // call sendCommand for each command and the output 
+	     //(without prompts) is returned
+	     String result = instance.sendCommand(command);
+	     // close only after all commands are sent
+	     instance.close();
+	     System.out.println("expected: " + expResult);
+	     System.out.println("Result: "+ result);
+	     
+	}
+
+}
